275d3f5940e14d3055450208e2680887f121960b
tangxiumei <tangxiumei@rfchina.com>
2018-12-06 09:48:11 +0800

    add selector sort function

--- dist/algorithm-sort.js
+++ dist/algorithm-sort.js
@@ -1,7 +1,7 @@
 function bubbule(arr) {
   for (var i = 0; i < arr.length - 1; i++) {
+    console.info(arr);
     for (var j = 0; j < arr.length - 1 - i; j++) {
-      // console.info(arr)
       var tmp = arr[j];
       if (arr[j] > arr[j + 1]) {
         arr[j] = arr[j + 1];
@@ -13,4 +13,23 @@ function bubbule(arr) {
   console.info('bubbule:', arr);
 }
 
-export default { bubbule: bubbule };
+function selectionSort(arr) {
+  for (var i = 0; i < arr.length - 1; i++) {
+    // console.info(arr)
+    for (var j = i + 1; j <= arr.length - 1; j++) {
+      if (arr[i] > arr[j]) {
+        var temp = void 0;
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+
+  console.info('selectionSort:', arr);
+}
+
+export default {
+  bubbule: bubbule,
+  selectionSort: selectionSort
+};
--- src/algorithm-sort.js
+++ src/algorithm-sort.js
@@ -1,11 +1,11 @@
 function bubbule(arr) {
-  for (let i =0; i < arr.length - 1; i++) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    console.info(arr)
     for (let j = 0; j < arr.length - 1 - i; j++) {
-      // console.info(arr)
       let tmp = arr[j]
-      if (arr[j] > arr[j+1]) {
-        arr[j] = arr[j+1]
-        arr[j+1] = tmp
+      if (arr[j] > arr[j + 1]) {
+        arr[j] = arr[j + 1]
+        arr[j + 1] = tmp
       }
     }
   }
@@ -13,6 +13,23 @@ function bubbule(arr) {
   console.info('bubbule:', arr)
 }
 
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    // console.info(arr)
+    for (let j = i + 1; j <= arr.length - 1; j++) {
+      if (arr[i] > arr[j]) {
+        let temp
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
 
+  console.info('selectionSort:', arr)
+}
 
-export default {bubbule}
+export default {
+  bubbule,
+  selectionSort
+}
