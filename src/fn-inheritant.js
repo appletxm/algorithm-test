91f6ae20e560f98e61b713b5faa1978a1e263fe4
xiumeitang <xiumeitang@airasia.com>
2022-01-13 10:48:02 +0800

    inheritant instance

--- src/fn-inheritant.js
+++ src/fn-inheritant.js
@@ -8,6 +8,10 @@ Range.prototype.squrt = function() {
   return this.width * this.height
 }
 
-function Range2() {}
-Range2.prototype = new Range(3, 5)
+function Range2(z) {
+  this.z = z
+}
+// Range2.prototype = new Range(3, 5)
+// Range2.prototype.constructor = Range2
+Range2.prototype.__proto__ = new Range(3, 5)
 Range2.prototype.test = function(){console.log('i am method from Range2')}
